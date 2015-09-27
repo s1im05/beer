b100400bc19dfc868638b78e7f8ff9a78e31499c
Vadim Suleimanov <s1imus05@gmail.com>
2015-09-27 14:46:53 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Beer</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/common.css" rel="stylesheet" />
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container b-main">
+        Main Layout
+    </div>
+</body>
+</html>
