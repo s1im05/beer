d6324b26522012b46a5df38a7aa459d02baa85ac
Vadim Suleimanov <me@vsul.ru>
2016-02-18 12:04:20 +0300

    Template for index page

--- lib/SSCE/controllers/index.class.php
+++ lib/SSCE/controllers/index.class.php
@@ -3,6 +3,8 @@ namespace SSCE\Controllers;
 
 class Index extends Base {
     
+    protected $_sTemplate   = 'index.tpl.php';
+    
     public function indexAction(){
         $aData  = $this->db->select("SELECT * FROM ?_sort WHERE `show` = 1 ORDER BY `sort` ASC;");
 
